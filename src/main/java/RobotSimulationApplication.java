9e128e544dabc0aea5ba8473bf03bb115a0bbb8a
vishwadavelma <50398006+vishwadavelma@users.noreply.github.com>
2022-07-29 13:51:21 +1000

    Update RobotSimulationApplication.java

--- src/main/java/RobotSimulationApplication.java
+++ src/main/java/RobotSimulationApplication.java
@@ -16,17 +16,6 @@ public class RobotSimulationApplication {
        int robotCount = 0;
        RobotModel activeRobot = null;
        List<RobotModel> robotsList = new ArrayList<>();
-       /*System.out.println("Welcome to the Robot Simulation!! \n" +
-               "You can simulate robot using the following commands \n" +
-               "1. PLACE X,Y,F --> where X,Y are positions on table top and F is direction(i.e NORTH,EAST,WEST,SOUTH) \n" +
-               "2. MOVE    --> moves the robot one step forward in the direction it is facing \n" +
-               "3. LEFT    --> rotates the robot in 90 degrees towards left without changing position \n" +
-               "4. RIGHT   --> rotates the robot in 90 degrees towards right without changing position \n" +
-               "5. REPORT  --> Displays the current position of the robot \n" +
-               "6. STOP    --> To Stop and Exit from the program \n" +
-               "7. ROBOT <NUMBER> --> Makes the robot identified by as active.\n\n" +
-               "NOTE: To start with, Please enter the 'PLACE X,Y,F' command to place the robot on the square table top and then proceed with other commands. \n" +
-               "Example: PLACE 1,1,NORTH\n");*/
        System.out.println(RobotCommonConstants.DISPLAY_MENU);
        while (true) {
            //Read the input commands from console.
@@ -52,14 +41,6 @@ public class RobotSimulationApplication {
                        //Displaying a message whenever the 1st Robot is placed on table to start the simulation
                        if(robotCount == 1) {
                            System.out.println(RobotCommonConstants.DISPLAY_MENU_AFTER_PLACE);
-                           /*System.out.println("Hurray!!Now you can enjoy simulating your robot using any of the below commands\n" +
-                                   "PLACE X.Y,F \n" +
-                                   "MOVE \n" +
-                                   "LEFT \n" +
-                                   "RIGHT \n" +
-                                   "REPORT \n" +
-                                   "ROBOT <number> \n" +
-                                   "STOP\n");*/
                            //By default, setting the first placed robot as active robot.
                            activeRobot = robotModel;
                        }
